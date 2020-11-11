271d5bf00b011dddedab498c00ca3f2f5ff1c134
Keesuk Lee <keesukee@naver.com>
2020-11-12 01:03:59 +0900

    테스트

---
+++ .history/src/App/Poster_20201111125618.js
@@ -0,0 +1,211 @@
+import React, { PureComponent } from 'react';
+import styled from 'styled-components';
+import { Circle, Triangle, Rhombus, Ellipse, Rectangular } from './_Diagram.jsx';
+import DataMap from './_DataMap.jsx';
+import { Dice } from './_Diagram.jsx';
+
+const PosterContentDiv = styled.div`
+
+    width: 40vw;
+    height: 60vw;
+    box-sizing: border-box;
+    background-color: white;
+    margin-left: 5vw;
+    margin-right: 5vw;
+    margin-top: 5vh;
+    margin-bottom: 5vh;
+
+    @media all and (min-width:0px) and (max-width:1023px) {
+        text-align: center;
+        width: 88vw;
+        height: 124.432vw;
+        background-color: white;
+        margin: auto;
+        margin-top: 2vw;
+    }
+`;
+const Logo = styled.img`
+
+    display: block;
+    margin: auto;
+    padding-top: 15vw;
+
+    @media all and (min-width:0px) and (max-width:1023px) {
+        padding-top: 4vw;
+        width: 76%;
+`;
+const QuestionDiv = styled.div`
+    
+    width: 88vw;
+    height: 88vw;
+`;
+const Question = styled.img`
+
+    display: block;
+    margin: auto;
+    padding-top: 17vw;
+    width: 55%;
+
+    @media all and (min-width:0px) and (max-width:1023px) {
+        padding-top: 15vw;
+        width: 20%;
+`;
+const TextDiv = styled.div`
+    
+    font-size: 4.4vw;
+    display: block;
+    margin-top: 19px;
+    margin-left: auto;
+    margin-right: auto;
+    font-weight: 700;
+    letter-spacing: -.2px;
+    width: 55vw;
+    line-height: 1.3;
+`;
+const MapExplainWrapperDiv = styled.div`
+
+    width: 99vw;
+    height: 300vh;
+    background-color: white;
+    margin-top: 11vh;
+`;
+const MapExplainDiv = styled.div`
+
+    width: 88vw;
+    margin: auto;
+`;
+const Bottom = styled.img`
+
+    display: block;
+    margin: auto;
+    padding-top: 15vw;
+    width: 20%;
+
+    @media all and (min-width:0px) and (max-width:1023px) {
+        padding-top: 5vw;
+        width: 12%;
+`;
+const ExplainTitle = styled.div`
+    font-size: 8.2vw;
+    letter-spacing: 0px;
+    font-weight: 900;
+    margin-top: 100px;
+`;
+const ExplainPlanning = styled.div`
+    font-size: 5.4vw;
+    letter-spacing: -.9px;
+    font-weight: 900;
+    margin-top: 20px;
+    line-height: 1.6;
+`;
+const MapShowText = styled.div`
+    font-size: 3.85vw;
+    margin-left: -4.5vw;
+    width: 100%;
+    font-weight: 900;
+    letter-spacing: -.6px;
+    display:flex; 
+    flex-direction:row;
+    white-space: nowrap;
+`;
+const WholeWrap = styled.div`
+    transition: all .1s ease;
+    transition-delay: 1s;
+`;
+
+class Poster extends PureComponent {
+    state={}
+
+    componentDidMount(){
+        this.setState({whole : false})
+        setTimeout(() => {this.setState({whole : true})},10)
+    }
+
+    handleChange = (diceOn) => {
+        this.setState({posterOrnamentOn: diceOn}) 
+        
+        setTimeout(()=> {
+            this.setState({posterOrnamentOn: false})
+        }, 500)
+    }
+
+    render(){
+        const { posterPin, pageUp } = this.props
+
+        return (
+            pageUp ?
+            <>
+                <PosterContentDiv>
+                    <WholeWrap style={this.state.whole
+                        ?{opacity:1}
+                        :{opacity:0}
+                    }>
+                        <Logo
+                            src={require('../data/Logo.svg')}
+                        />
+                        {posterPin !== Array(0)
+                        ? 
+                        <DataMap 
+                            sclae={1}
+                            panOff={true}
+                            stroke={true}
+                            pointer={false}
+                            posterOn={pageUp}
+                            rerender={this.state.posterOrnamentOn}
+                        />
+                        : 
+                        <QuestionDiv>
+                            <Question src={require('../data/QuestionMark.svg')}/>
+                            <TextDiv> 내가 본 서울시가 완성되면,<br/>우리가 본 서울시가 보입니다.</TextDiv>
+                        </QuestionDiv>
+                        }
+                        {posterPin !== Array(0)
+                        ?<Dice
+                            changeOn={this.handleChange}
+                        />
+                        :null}
+                    </WholeWrap>
+                </PosterContentDiv>
+                <MapExplainWrapperDiv>
+                    <MapExplainDiv>
+                        <Bottom src={require('../data/Bottom.svg')}/>
+                        {posterPin !== Array(0)
+                        ? <>
+                            <ExplainTitle>
+                                서울은 이런 곳.
+                            </ExplainTitle>
+                            <DataMap 
+                                scale={5}
+                                panOff={false}
+                                stroke={false}
+                                pointer={true}
+                                posterPin={ posterPin[0] }
+                            />
+                            <MapShowText>
+                                <Rhombus right={'0px'}/>
+                                    <div style={{color:'#e90087'}}>박물관 갤러리</div>
+                                <Rectangular right={'0px'}/>
+                                    <div style={{color:'#006CC4'}}>쇼핑 마켓</div>
+                                <Circle right={'0px'}/>
+                                    <div style={{color:'#E20000'}}>랜드마크</div>
+                                <Triangle right={'0px'} top={'-0.5px'}/>
+                                    <div style={{color:'#087A0D'}}>자연 휴양지</div>
+                                <Ellipse right={'0px'} top={'-4.5px'}/>
+                                    <div style={{color:'#00bfb3'}}>음식점</div>
+                            </MapShowText>
+                        </>
+                        : null}
+                        <ExplainTitle>
+                            기획의도
+                        </ExplainTitle>
+                            <ExplainPlanning>
+                                지역은 개개인의 경험 속 저마다의 다른 의미가 존재합니다. 그렇기 때문에 내가 보는 특정 지역은 타인이 보는 관점과 다를 수도 같을 수도 있습니다. <br/> 우리가 본 서울시 프로젝트는 이러한 아이디어에 착안하여 제작되었습니다. 대한민국을 대표하는 도시 서울은 방대한 사람들이 오가는 곳이기에 타도시에 비해 비교적 넓은 공감대를 지니고 있습니다. 그렇기 때문에 서울시는 특정지역을 보더라도 다양한 관점이 나올 수 있고, 그 속에서 공감 혹은 낯선 감정들을 느낄 수 있습니다. <br/> 우리가 본 서울시 프로젝트는 저마다의 다른 관점, 추억, 경험을 재구성하여 공감이 갈 수 있는 데이터 시각화를 목표로 하며, 여러 사람들의 데이터를 융합한 공감지도를 그려보고자 합니다.
+                            </ExplainPlanning>
+                    </MapExplainDiv>
+                </MapExplainWrapperDiv>
+            </>
+            :null
+        )
+    }
+}
+export default Poster;
---
+++ .history/src/App/_DataMap_20201111232642.jsx
@@ -0,0 +1,247 @@
+import React, {PureComponent} from "react";
+import styled from 'styled-components';
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
+import { pxStringfier, colorChooser, randomize, mapDrawer, infoGraphic, getRandomInt } from "./_Diagram.jsx";
+
+
+const ExplainDiv = styled.div`
+    position: absolute;
+    z-index: 1000;
+    margin-top: 2vw;
+    box-shadow: 5px 5px #333;
+    box-sizing: border-box;
+    border: 1.5px solid #333;
+    background-color: white;
+    
+    @media all and (min-width:0px) and (max-width:1023px) {
+        width: 40vw;
+        height: 40vw;
+    }
+`;
+const ExplainTitle = styled.div`
+    font-size: 5vw;
+    margin-left: 2vw;
+    margin-top: 1vw;
+    letter-spacing: -.2vw;
+    font-weight: 900;
+`;
+const ExplainNumber = styled.div`
+    font-size: 3.6vw;
+    font-weight: 900;
+    line-height: 5.2vw;
+    position: absolute;
+    right: 2vw;
+`;
+const ExplainDiagram = styled.div`
+    width: 29vw;
+    margin-left: .5vw;
+    height: 30vw;
+`;
+const Diagram = styled.div`
+    position: absolute;
+    left: ${props => props.left};
+    margin-top:${props => props.top};
+    font-size: ${props => props.size};
+    color: ${props => props.color};
+    mix-blend-mode: multiply;
+`;
+const SvgContainer = styled.div`
+    display: inline-block;
+    position: relative;
+    cursor: move;
+
+    @media all and (min-width:0px) and (max-width:1023px) {
+        display: inline-block;
+        position: relative;
+        width: 88vw;
+        height: 88vw;
+    }
+`;
+const Svg = styled.svg`
+
+    .cls-1 {
+        font-family:'Noto Sans KR'; 
+        font-weight:900; 
+        cursor: ${props => props.pointer ? 'pointer' : 'move'};
+        pointer-events: ${props => props.pointer ? '' : 'none'};
+        stroke-width:${props => props.stroke ? '1.5px' : '0px'};
+        stroke: #999;
+        font-size: 6.39px;
+        letter-spacing: -0.04em;
+    }
+    .cls-2 {
+        font-family:'Noto Sans KR'; 
+        font-weight:900; 
+        cursor: ${props => props.pointer ? 'pointer' : 'move'};
+        pointer-events: ${props => props.pointer ? '' : 'none'};
+        stroke-width:${props => props.stroke ? '1.5px' : '0px'};
+        stroke: #999;
+        font-size: 4.78px;
+        letter-spacing: -0.03em;
+    }
+    .purple{
+        font-family:'Noto Sans KR'; 
+        font-weight:900; 
+        fill:#e90087;
+        font-size: 30px;
+    }
+    .blue{
+        font-family:'Noto Sans KR'; 
+        font-weight:900; 
+        fill:#006CC4;
+        font-size: 30px;
+    }
+    .red{
+        font-family:'Noto Sans KR'; 
+        font-weight:900; 
+        fill:#E20000;
+        font-size: 30px;
+    }
+    .green{
+        font-family:'Noto Sans KR'; 
+        font-weight:900; 
+        fill:#087A0D;
+        font-size: 30px;
+    }
+    .yellow{
+        font-family:'Noto Sans KR'; 
+        font-weight:900; 
+        fill:#00bfb3;
+        font-size: 30px;
+    }
+`;
+
+const location = {
+    "Bulgwang":{"0":52,"1":44,"2":66,"3":66,"4":88,"kor":"불광"},
+    "Euljiro4saga":{"0":59,"1":77,"2":88,"3":55,"4":88,"kor":"을지로4가"},
+    "Noryangjin":{"0":98,"1":76,"2":85,"3":66,"4":86,"kor":"노량진"},
+    "Sujiguoffice":{"0":60,"1":52,"2":76,"3":75,"4":51,"kor":"수지구청"},
+    "Wolgye":{"0":88,"1":75,"2":75,"3":34,"4":75,"kor":"월계"}
+    }
+
+const colorList = ['#e90087','#006CC4','#E20000','#087A0D','#00bfb3' ]
+const distanceLeftList  = ['7vw', '8vw', '9vw', '10vw', '11vw', '12vw']
+const distanceTopList  = ['2vh','2.5vh', '3vh', '3.5vh','4vh']
+const diagramList = ['◆', '■', '●', '▲', '⬮']
+
+class MapShow extends PureComponent {
+    constructor(props){
+        super(props)
+        this.state={
+            purple: 50,
+            blue: 20,
+            red: 20,
+            green: 9,
+            yellow: 1,
+            location: '',
+            dataLocation : [],
+            corArr: [],
+        }
+        this.cor = []
+        this.getLocation = this.getLocation.bind(this)
+    }
+
+    componentDidMount(){
+        this.setState({ location : this.props.posterPin})
+        this.parsingCor()
+
+        setTimeout(() => {
+            let num = getRandomInt(0, 500)
+            let one = this.cor
+        console.log(one)
+            }, 1000)
+    }
+
+    parsingCor(){
+        const parseCor = this.state.dataLocation.map(station => this.cor[station.name])
+            .map(station => station.innerHTML
+            .split('(').pop().split(')')[0].split(' '))
+            .map(station => [parseInt(station[0]),parseInt(station[1])])
+        this.setState({ corArr : parseCor })
+    }
+
+    getLocation(value){
+        this.setState({ location : value})
+    } 
+
+    render() {
+        const { scale, panOff, stroke, pointer, posterOn } = this.props
+
+        return (
+                <>
+                {stroke ? null :
+                    <ExplainDiv>
+                        <ExplainTitle>
+                            {this.state.location}
+                        </ExplainTitle>
+                        <ExplainNumber>
+                            {colorList.map(item => 
+                                <div 
+                                    key={item} 
+                                    style={{color: item}}
+                                >
+                                {this.state[colorChooser(item)]}%
+                                </div>
+                            )}
+                        </ExplainNumber>
+                        <ExplainDiagram>
+                            {colorList.map((item, i) => 
+                                <Diagram 
+                                    color={colorList[i]} 
+                                    size={pxStringfier(this.state[colorChooser(item)])}
+                                    top={randomize(distanceTopList)}
+                                    left={randomize(distanceLeftList)}
+                                    key={item}
+                                >
+                                {diagramList[i]}
+                                </Diagram>
+                            )}
+                        </ExplainDiagram>
+                    </ExplainDiv>
+                }
+                <TransformWrapper
+                    scale={scale}
+                    wheel={{disabled: true}}
+                    pan={{disabled: panOff}}
+                >
+                    {({ resetTransform, scale, ...rest }) => (
+                    <React.Fragment>
+                        <TransformComponent>
+                            <SvgContainer> 
+                                <Svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    x="0"
+                                    y="0"
+                                    enableBackground="new 0 0 1280 1280"
+                                    version="1.1"
+                                    viewBox="0 0 1280 1280"
+                                    xmlSpace="preserve"
+                                    stroke={stroke ? 1 : 0}
+                                    pointer={pointer ? 1 : 0}
+                                >
+                                {stroke 
+                                ? <g transform={'translate(10, 30) scale(.7)'}>
+                                    <text transform="translate(80 30)" className="purple">&#9670; 박물관 갤러리</text>
+                                    <text transform="translate(80 90)" className="green">▲ 자연 휴양지</text>
+                                    <text transform="translate(80 150)" className="blue">■ 쇼핑 마켓</text>
+                                    <text transform="translate(80 210)" className="red">● 랜드마크</text>
+                                    <text transform="translate(80 270)" className="yellow">⬮  음식점</text>
+                                </g>
+                                : null}
+                                {mapDrawer(this.getLocation, this.cor)}
+                                {posterOn ? 
+                                <g style={{mixBlendMode: 'multiply'}}>
+                                    {this.state.dataLocation !== [] ? this.state.dataLocation.map((one, i) => infoGraphic(one, this.state.corArr[i], i)) : null}
+                                </g>
+                                : null}
+                            </Svg>
+                            </SvgContainer>
+                        </TransformComponent>
+                    </React.Fragment>
+                    )}
+                </TransformWrapper>
+            </>
+        )
+    }
+}
+export default MapShow;
